41a1a08ff32e2ba9714f02736cff0f50071c6b3f
Reka Szabo <reka.szabo@adas639linux.ad.adasworks.com>
2022-04-27 08:44:59 +0200

    main window created

--- bpe.py
+++ bpe.py
@@ -1,1 +1,30 @@
-import tkinter
+from tkinter import *
+
+#implementing methods for functionality
+
+#main 
+
+def open_editor():
+    btn_open.pack_forget()
+
+    menu = Menu(root)
+    root.config(menu=menu)
+    filemenu = Menu(menu)
+    menu.add_cascade(label='File', menu=filemenu)
+    filemenu.add_command(label='New')
+    filemenu.add_command(label='Open...')
+    filemenu.add_separator()
+    filemenu.add_command(label='Exit', command=root.quit)
+    helpmenu = Menu(menu)
+    menu.add_cascade(label='Help', menu=helpmenu)
+    helpmenu.add_command(label='About')
+    mainloop()
+
+
+
+root = Tk()
+root.geometry('800x600')
+root.title('Photo editor for beginners')
+btn_open = Button(root, text='Open', width=25, command=open_editor)
+btn_open.pack()
+mainloop()
